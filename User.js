4d1806b56fa9b5666aef0555a7ec942435c29313
Vanshika Verma <itsvanshikaverma03@gmail.com>
2024-12-14 18:33:48 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
